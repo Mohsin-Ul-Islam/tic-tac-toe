9f8872c30e962f2de68bc0729dcc673de6589f19
Mohsin-Ul-Islam <mohsinulislam180@gmail.com>
2023-07-05 17:11:18 +0500

    refactor: dynamically create cell nodes

--- public/script.js
+++ public/script.js
@@ -1,95 +1,110 @@
-let turn = 0;
-let gameOver = false;
-let boardSize = 3;
 
-const log = document.querySelector('#log');
-const board = Array(boardSize * boardSize).fill(NaN);
+window.addEventListener('load', () => {
 
-document.querySelectorAll(".cell").forEach((cell, i) => {
-    cell.addEventListener('click', () => {
+    // mutable game state variables
+    let turn = 0;
+    let boardSize = 3;
+    let gameOver = false;
 
-        // cell already occupied
-        if (!isNaN(board[i]) || gameOver) {
-            cell.style.animation = 'shake 0.5s';
-            setTimeout(() => { cell.style.animation = 'none'; }, 500)
-            return;
-        }
+    // references to html elements
+    const logEl = document.getElementById('log');
+    const boardEl = document.getElementById('board');
 
-        // game already over
-        if (gameOver) {
-            return;
-        }
+    // the in memory tic tac toe board
+    const board = Array(boardSize * boardSize).fill(NaN);
 
-        board[i] = turn;
-        if (turn === 0) {
-            // player 1's symbol
-            cell.classList.add('cell--crossed');
-        } else {
-            // player 2's symbol
-            cell.classList.add('cell--zeroed');
-        }
+    // create and inject cell elements into the DOM
+    board.forEach((_, i) => {
 
-        // check draw condition
-        if (board.every(idx => !isNaN(idx))) {
-            log.textContent = 'game drew!';
-            gameOver = true;
-            return;
-        }
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
 
-        let primaryDiagonalSum = 0;
-        let secondaryDiagonalSum = 0;
+        cell.addEventListener('click', (evt) => {
 
-        // check win condition
-        for (let i = 0; i < boardSize; i++) {
+            // prevent the browser's default click listener
+            evt.preventDefault();
 
-            let rowSum = 0;
-            let colSum = 0;
-
-            for (let j = 0; j < boardSize; j++) {
-
-                const rowIdx = i * boardSize + j;
-                const colIdx = j * boardSize + i;
-
-                rowSum += board[rowIdx];
-                colSum += board[colIdx];
-
-                if (i == j) {
-                    primaryDiagonalSum += board[rowIdx];
-                }
-
-                if (boardSize - 1 - i == j) {
-                    secondaryDiagonalSum += board[colIdx];
-                }
-
+            // if the cell is already occupied or the game is over
+            if (!isNaN(board[i]) || gameOver) {
+                cell.style.animation = 'shake 0.5s';
+                setTimeout(() => { cell.style.animation = 'none'; }, 500)
+                return;
             }
 
-            if (rowSum == 0 || colSum == 0) {
-                log.textContent = `Player 1 won!`
-                gameOver = true;
-                return;
-            } else if (rowSum == boardSize || colSum == boardSize) {
-                log.textContent = `Player 2 won!`
+            board[i] = turn;
+            if (turn === 0) {
+                // player 1's symbol
+                cell.classList.add('cell--crossed');
+            } else {
+                // player 2's symbol
+                cell.classList.add('cell--zeroed');
+            }
+
+            // check draw condition
+            if (board.every(idx => !isNaN(idx))) {
+                logEl.textContent = 'game drew!';
                 gameOver = true;
                 return;
             }
-        }
 
-        if (primaryDiagonalSum == 0 || secondaryDiagonalSum == 0) {
-            log.textContent = `Player 1 won!`
-            gameOver = true;
-            return;
-        }
+            let primaryDiagonalSum = 0;
+            let secondaryDiagonalSum = 0;
 
-        if (primaryDiagonalSum == boardSize || secondaryDiagonalSum == boardSize) {
-            log.textContent = `Player 2 won!`
-            gameOver = true;
-            return;
-        }
+            // check win condition
+            for (let i = 0; i < boardSize; i++) {
 
-        // change game turn
-        turn = (turn + 1) % 2;
-        log.textContent = `Player ${turn + 1}'s turn`
+                let rowSum = 0;
+                let colSum = 0;
 
-    })
-})
+                for (let j = 0; j < boardSize; j++) {
+
+                    const rowIdx = i * boardSize + j;
+                    const colIdx = j * boardSize + i;
+
+                    rowSum += board[rowIdx];
+                    colSum += board[colIdx];
+
+                    if (i == j) {
+                        primaryDiagonalSum += board[rowIdx];
+                    }
+
+                    if (boardSize - 1 - i == j) {
+                        secondaryDiagonalSum += board[colIdx];
+                    }
+
+                }
+
+                if (rowSum == 0 || colSum == 0) {
+                    logEl.textContent = `Player 1 won!`
+                    gameOver = true;
+                    return;
+                } else if (rowSum == boardSize || colSum == boardSize) {
+                    logEl.textContent = `Player 2 won!`
+                    gameOver = true;
+                    return;
+                }
+            }
+
+            if (primaryDiagonalSum == 0 || secondaryDiagonalSum == 0) {
+                logEl.textContent = `Player 1 won!`
+                gameOver = true;
+                return;
+            }
+
+            if (primaryDiagonalSum == boardSize || secondaryDiagonalSum == boardSize) {
+                logEl.textContent = `Player 2 won!`
+                gameOver = true;
+                return;
+            }
+
+            // change game turn
+            turn = (turn + 1) % 2;
+            logEl.textContent = `Player ${turn + 1}'s turn`
+
+        });
+
+        boardEl.appendChild(cell);
+    });
+
+});
 
