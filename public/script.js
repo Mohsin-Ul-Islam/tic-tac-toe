0055b30ad953b262a5e22c998dad3273888fc571
Mohsin-Ul-Islam <mohsinulislam180@gmail.com>
2023-07-05 16:47:47 +0500

    feat: add diagonal win conditions

--- public/script.js
+++ public/script.js
@@ -1,22 +1,26 @@
 let turn = 0;
+let gameOver = false;
 let boardSize = 3;
 
-const board = Array(boardSize * boardSize).fill(NaN);
 const log = document.querySelector('#log');
+const board = Array(boardSize * boardSize).fill(NaN);
 
 document.querySelectorAll(".cell").forEach((cell, i) => {
     cell.addEventListener('click', () => {
 
         // cell already occupied
-        if (!isNaN(board[i])) {
+        if (!isNaN(board[i]) || gameOver) {
             cell.style.animation = 'shake 0.5s';
             setTimeout(() => { cell.style.animation = 'none'; }, 500)
             return;
         }
 
-        console.log(`cell number ${i + 1} clicked by player ${turn + 1}!`);
-        board[i] = turn;
+        // game already over
+        if (gameOver) {
+            return;
+        }
 
+        board[i] = turn;
         if (turn === 0) {
             // player 1's symbol
             cell.classList.add('cell--crossed');
@@ -28,16 +32,18 @@ document.querySelectorAll(".cell").forEach((cell, i) => {
         // check draw condition
         if (board.every(idx => !isNaN(idx))) {
             log.textContent = 'game drew!';
+            gameOver = true;
             return;
         }
+
+        let primaryDiagonalSum = 0;
+        let secondaryDiagonalSum = 0;
 
         // check win condition
         for (let i = 0; i < boardSize; i++) {
 
             let rowSum = 0;
             let colSum = 0;
-            let primaryDiagonalSum = 0;
-            let secondaryDiagonalSum = 0;
 
             for (let j = 0; j < boardSize; j++) {
 
@@ -57,13 +63,27 @@ document.querySelectorAll(".cell").forEach((cell, i) => {
 
             }
 
-            if (rowSum == 0 || colSum == 0 || primaryDiagonalSum == 0 || secondaryDiagonalSum == 0) {
+            if (rowSum == 0 || colSum == 0) {
                 log.textContent = `Player 1 won!`
+                gameOver = true;
                 return;
-            } else if (rowSum == boardSize || colSum == boardSize || primaryDiagonalSum == boardSize || secondaryDiagonalSum == boardSize) {
+            } else if (rowSum == boardSize || colSum == boardSize) {
                 log.textContent = `Player 2 won!`
+                gameOver = true;
                 return;
             }
+        }
+
+        if (primaryDiagonalSum == 0 || secondaryDiagonalSum == 0) {
+            log.textContent = `Player 1 won!`
+            gameOver = true;
+            return;
+        }
+
+        if (primaryDiagonalSum == boardSize || secondaryDiagonalSum == boardSize) {
+            log.textContent = `Player 2 won!`
+            gameOver = true;
+            return;
         }
 
         // change game turn
